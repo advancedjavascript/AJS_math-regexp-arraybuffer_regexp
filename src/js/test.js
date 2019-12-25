141008de3d9abda437ebee401ec7990df7d2f187
kate_gaw <ekaterinagaw@gmail.com>
2019-12-25 14:22:16 +0300

    Change regexp

--- src/js/test.js
+++ src/js/test.js
@@ -1,38 +1,38 @@
 import Validator from './app';
 
 test('test uncorrect name', () => {
-  const name = '_user';
-  const result = Validator.validateUsername(name);
-  expect(result).toBe(false);
+    const name = '_user';
+    const result = Validator.validateUsername(name);
+    expect(result).toBe(false);
 });
 
 test('test uncorrect name', () => {
-  const name = '9user';
-  const result = Validator.validateUsername(name);
-  expect(result).toBe(false);
+    const name = '9user';
+    const result = Validator.validateUsername(name);
+    expect(result).toBe(false);
 });
 
 test('test uncorrect name', () => {
-  const name = 'пользователь';
-  const result = Validator.validateUsername(name);
-  expect(result).toBe(false);
+    const name = 'пользователь';
+    const result = Validator.validateUsername(name);
+    expect(result).toBe(false);
 });
 
 test('test uncorrect name', () => {
-  const name = 'user00000';
-  const result = Validator.validateUsername(name);
-  expect(result).toBe(false);
+    const name = 'user00000user';
+    const result = Validator.validateUsername(name);
+    expect(result).toBe(false);
 });
 
 
 test('test correct name', () => {
-  const name = 'kate-gaw';
-  const result = Validator.validateUsername(name);
-  expect(result).toBe(true);
+    const name = 'kate-gaw';
+    const result = Validator.validateUsername(name);
+    expect(result).toBe(true);
 });
 
-test('test correct name', () => {
-  const name = 'user98_pl12';
-  const result = Validator.validateUsername(name);
-  expect(result).toBe(true);
+test('test uncorrect name', () => {
+    const name = 'R2D2';
+    const result = Validator.validateUsername(name);
+    expect(result).toBe(false);
 });
