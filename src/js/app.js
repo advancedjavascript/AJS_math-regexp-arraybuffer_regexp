da2cad576f2901c2dfe8657ae6f9e37cbae14a9c
kate_gaw <ekaterinagaw@gmail.com>
2019-12-25 14:23:23 +0300

    Changed regexp

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 export default class Validator {
-    static validateUsername(name) {
-        const valideName = /^[a-z0-9_-]+$/i;
-        const invalidSymbols = /^(\b[0-9_]|[-])+|(\d{3,})+|([0-9_]|[-])+$/g;
-        // const invalidSymbol = /(\b[0-9_])/; // first & last symbol
-        // const invalidDash1 = /-$/; // if last symbol is -
-        // const invalidDash2 = /^-/; // if first symbol is -
-        // const invalidNum = /\d{3,}/; // more than 3 numbers
+  static validateUsername(name) {
+    const valideName = /^[a-z0-9_-]+$/i;
+    const invalidSymbols = /^(\b[0-9_]|[-])+|(\d{3,})+|([0-9_]|[-])+$/g;
+    // const invalidSymbol = /(\b[0-9_])/; // first & last symbol
+    // const invalidDash1 = /-$/; // if last symbol is -
+    // const invalidDash2 = /^-/; // if first symbol is -
+    // const invalidNum = /\d{3,}/; // more than 3 numbers
 
-        return (valideName.test(name) &&
-            !(invalidSymbols.test(name)));
-    }
+    return (valideName.test(name)
+            && !(invalidSymbols.test(name)));
+  }
 }
